6925c7813f69dc4da17d0dd2f64f289c585e4b6a
Aman-Kasa <a.kasa@alustudent.com>
2026-08-31 11:51:13 +0200

    Add EC key pair saving implementation

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -8,6 +8,12 @@
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/obj_mac.h>
+#include <openssl/pem.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 #ifndef SHA256_DIGEST_LENGTH
 #define SHA256_DIGEST_LENGTH 32
@@ -26,5 +32,6 @@ uint8_t *sha256(int8_t const *s, size_t len,
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
 
 #endif /* HBLK_CRYPTO_H */
---
+++ test/ec_save-main.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hblk_crypto.h"
+
+void _print_hex_buffer(uint8_t const *buf, size_t len);
+
+/**
+ * main - Entry point
+ * @ac: Arguments count
+ * @av: Arguments vector
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int ac, char **av)
+{
+	EC_KEY *key;
+	uint8_t pub[EC_PUB_LEN];
+
+	if (ac < 2)
+	{
+		fprintf(stderr, "Usage: %s <path>\n", av[0]);
+		return (EXIT_FAILURE);
+	}
+
+	key = ec_create();
+	if (!key)
+	{
+		fprintf(stderr, "ec_create() failed\n");
+		return (EXIT_FAILURE);
+	}
+
+	ec_to_pub(key, pub);
+	printf("Public key: ");
+	_print_hex_buffer(pub, EC_PUB_LEN);
+	printf("\n");
+
+	/* Test `ec_save()` */
+	if (!ec_save(key, av[1]))
+	{
+		fprintf(stderr, "ec_save() failed\n");
+		EC_KEY_free(key);
+		return (EXIT_FAILURE);
+	}
+
+	printf("Successfully saved keys to %s\n", av[1]);
+
+	/* Cleanup */
+	EC_KEY_free(key);
+	return (EXIT_SUCCESS);
+}
