e9eedeb96922a581db8ff025840375745e3be5bc
Aman-Kasa <a.kasa@alustudent.com>
2026-08-31 11:48:56 +0200

    Add EC public key extraction implementation

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -17,8 +17,13 @@
 #define EC_CURVE NID_secp256k1
 #endif
 
+#ifndef EC_PUB_LEN
+#define EC_PUB_LEN 65
+#endif
+
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 #endif /* HBLK_CRYPTO_H */
